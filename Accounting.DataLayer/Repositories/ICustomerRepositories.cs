8914910c2491638464205bb47dd24809c1f24e00
Danial <D.roshanfekr@gmial.com>
2023-01-23 10:19:44 +0330

    Defining methods for user to let him/her use the data base.

---
+++ Accounting.DataLayer/Repositories/ICustomerRepositories.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Repositories
+{
+    public interface ICustomerRepositories
+    {
+        List<Customers> GetAllCustomers();
+        Customers GetCustomersById(int customerID);
+        bool InsertCustomer(Customers customer);
+        bool UpdateCustomer(Customers customer);
+
+        bool DeleteCustomer(Customers customer);
+        bool DeleteCustomer(int customerID);
+        void save();
+    }
+}
---
+++ Accounting.DataLayer/Services/CustomersRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Accounting.DataLayer.Repositories;
+
+namespace Accounting.DataLayer.Services
+{
+    public class CustomersRepository : ICustomerRepositories
+    {
+        Accounting_DBEntities db = new Accounting_DBEntities();
+        public bool DeleteCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = System.Data.Entity.EntityState.Deleted;
+                return true;
+
+            }
+            catch
+            { return false; }
+        }
+
+        public bool DeleteCustomer(int customerID)
+        {
+            try
+            {
+                var customer = GetCustomersById(customerID);
+                DeleteCustomer(customer);
+                return true;
+
+            }
+            catch
+            { return false; }
+        }
+
+        public List<Customers> GetAllCustomers()
+        {
+            return db.Customers.ToList();
+        }
+
+        public Customers GetCustomersById(int customerID)
+        {
+            return db.Customers.Find(customerID);
+        }
+
+        public bool InsertCustomer(Customers customer)
+        {
+            try
+            {
+                db.Customers.Add(customer);
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool UpdateCustomer(Customers customer)
+        {
+            try
+            {
+                db.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+                return true;
+
+            }
+            catch
+            { return false; }
+
+
+        }
+
+        public void save()
+        {
+            db.SaveChanges();
+        }
+
+
+    }
+}
